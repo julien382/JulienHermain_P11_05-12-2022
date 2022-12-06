8fa929cd703f24b8e17cc52250ebfb4aca8ad771
julien382 <julhermain@gmail.com>
2022-12-06 16:43:08 +0100

    create sectionImage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
-import Banner from './Banner/Banner'
-import Card from './Card/Card'
 import '../styles/index.css';
 import '../styles/reset.css';
+import Banner from './Banner/Banner'
+import Card from './Card/Card'
+import SectionImage from './SectionImage/SectionImage';
 
 function App() {
     return (
-        <div id='appContainer'>
+        <div id='app'>
             <Banner />
+            <SectionImage />
             <Card />
         </div>
     )
---
+++ src/Components/SectionImageTitle/SectionImageTitle.js
@@ -0,0 +1,11 @@
+import './SectionImageTitle.css'
+
+const SectionImageTitle = ({title}) => {
+    return (
+        <p className='sectionImageTitle'>
+            {title}
+        </p>
+    )
+}
+
+export default SectionImageTitle
