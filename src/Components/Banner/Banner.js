239395bc19e1930afd14bd7db733423b8d30d287
julien382 <julhermain@gmail.com>
2022-12-07 00:33:09 +0100

    banner image

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,8 +1,10 @@
 import './Banner.css'
+import bannerImage from "../../assets/montagne.png"
 
 const Banner = () => {
     return (
         <div className='banner'>
+            <img src={bannerImage} alt='bannerImage' className='bannerImage' />
             <h1 className="bannerTitle">Chez vous, partout et ailleurs</h1>
         </div>
     )
