178f9423a969642a124a396415cffcf71605cabf
julien382 <julhermain@gmail.com>
2022-12-09 23:58:03 +0100

    cardTitle ok

--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -8,8 +8,10 @@ const Card = () => {
         <div className='cards'>
             {data.map((logement) => (
                 <Link className='card' to={`FicheLogement/${logement.id}`}>
-                    <article className="cardImage">
-                        <img src={logement.cover} alt="logement" />
+                    <article>
+                        <div className="cardImage">
+                            <img src={logement.cover} alt="logement" />
+                        </div>
                         <h2 className='cardTitle'>{logement.title}</h2>
                     </article>
                 </Link>
