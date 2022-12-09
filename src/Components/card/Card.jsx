6437716803affd7d5a21503b39ae1ffcd3e3872f
julien382 <julhermain@gmail.com>
2022-12-09 23:23:32 +0100

    import data title/image et css image

--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -1,38 +1,20 @@
 import './Card.css'
 import '../../styles/mediaQueries.css'
-import CardTitle from "../CardTitle/CardTitle"
 import data from '../../data/data.json'
+import {Link} from "react-router-dom"
 
 const Card = () => {
     return (
         <div className='cards'>
-            <div className='card'>
-                <CardTitle title="Titre de la location"/>
-                
-            </div>
-            <div className='card'>
-                <CardTitle title="Bruxelles"/>
-                
-            </div>
-            <div className='card'>
-                <CardTitle title="Paris"/>
-                
-            </div>
-            <div className='card'>
-                <CardTitle title="Londres"/>
-                
-            </div>
-            <div className='card'>
-                <CardTitle title="Tokyo"/>
-                
-            </div>
-            <div className='card'>
-                {data.map(logement => (
-                    <CardTitle title={logement.title}/>
-                ))}
-                
-            </div>
-
+            {data.map((logement) => (
+                <Link className='card' to={`FicheLogement/${logement.id}`}>
+                    <article className="cardImage">
+                        <img src={logement.cover} alt="logement" />
+                        <h2 className='cardTitle'>{logement.title}</h2>
+                    </article>
+                </Link>
+            ))}
+            
         </div>
     )
 }
