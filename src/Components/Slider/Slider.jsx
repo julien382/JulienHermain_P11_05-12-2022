89c50680fb095aae7f3b5af216e651f1fe9a2170
julien382 <julhermain@gmail.com>
2022-12-10 22:20:53 +0100

    ficheLogement récup de l'id de l'url

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -5,15 +5,15 @@ import data from '../../data/data.json'
 const Slider = () => {
     const url = window.location.href
     const finUrl = url.split('/').pop()
-    
+
     return (
         <div className='Slider'>
             {data.map((logement) => (
                 <div className="cardImage">
-                if ({logement.id} == {finUrl}) {
-                <img src={logement.pictures} alt="logement" />
-                }
-            </div>
+                    if ({logement.id} == {finUrl}) {
+                        <img src={logement.pictures} alt="logement" />
+                    }
+                </div>
             ))}
         </div>
     )
