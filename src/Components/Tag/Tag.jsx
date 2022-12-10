7757089b59b6fbb0edf1316507f56b636c4b9576
julien382 <julhermain@gmail.com>
2022-12-10 14:08:56 +0100

    creat Tag

---
+++ src/Components/Tag/Tag.jsx
@@ -0,0 +1,18 @@
+import './Tag.css'
+import '../../styles/mediaQueries.css'
+
+const Tag = () => {
+    return (
+        <div className='tags'>
+            <div className='tag'>
+                <h3 className='tagTitle'>Cozy</h3>
+            </div>
+            <div className='tag'>
+                <h3 className='tagTitle'>Cozy</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Tag
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,13 @@ import Banner from '../../Components/Banner/Banner.jsx'
 import Card from '../../Components/Card/Card.jsx'
 import montagne from "../../assets/montagne.png"
 import CardDescription from '../../Components/CardDescription/CardDescription.jsx'
+import Tag from '../../Components/Tag/Tag.jsx'
 
 const Home = () => {
     return (
         <div>
             <Banner img={montagne} title="Chez vous, partout et ailleurs"/>
+            <Tag />
             <CardDescription />
             <Card />
         </div>
