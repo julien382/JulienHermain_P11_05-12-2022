72b61a8a7b6c6e68386b57b63abb1dadd8195381
julien382 <julhermain@gmail.com>
2022-12-10 13:32:46 +0100

    cardDesciption zdd

--- src/Components/CardDescription/CardDescription.jsx
+++ src/Components/CardDescription/CardDescription.jsx
@@ -1,5 +1,6 @@
 import './CardDescription.css'
 import '../../styles/mediaQueries.css'
+import arrow from "../../assets/arrow.svg"
 
 const CardDescription = () => {
     return (
@@ -11,6 +12,7 @@ const CardDescription = () => {
                     <div className='blocCardDescriptionTexte'>
                         <p className='cardDescriptionTexte'>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
                     </div>
+                    <img src={arrow} alt='arrow'></img>
                 </div>
             </div>
             
@@ -21,6 +23,7 @@ const CardDescription = () => {
                     <div className='blocCardDescriptionTexte'>
                         <p className='cardDescriptionTexte'>Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied). </p>
                     </div>
+                    <img src={arrow} alt='arrow'></img>
                 </div>
             </div>
 
