0c39694f96d47cc7b303f9c38be130e801c88820
julien382 <julhermain@gmail.com>
2022-12-10 12:22:27 +0100

    create cardDesciption

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
 import Banner from '../../Components/Banner/Banner.jsx'
 import Card from '../../Components/Card/Card.jsx'
 import montagne from "../../assets/montagne.png"
+import CardDescription from '../../Components/CardDescription/CardDescription.jsx'
 
 const Home = () => {
     return (
         <div>
             <Banner img={montagne} title="Chez vous, partout et ailleurs"/>
+            <CardDescription />
             <Card />
-            
         </div>
     )
 }
