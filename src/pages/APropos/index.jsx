ea3bdb5c22cc9a489ea8cb3b7481e009250bfb82
julien382 <julhermain@gmail.com>
2022-12-16 17:36:43 +0100

    about taille des cardDescription

--- src/pages/APropos/index.jsx
+++ src/pages/APropos/index.jsx
@@ -1,11 +1,17 @@
 import './APropos.css'
 import Banner from '../../Components/Banner/Banner.jsx'
 import montagneNeige from "../../assets/montagneNeige.png"
+import CardDescription from '../../Components/CardDescription/CardDescription'
 
 const APropos = () => {
     return (
         <div className='pageAPropos'>
             <Banner img={montagneNeige}/>
+
+            <div className='aProposCardsDescription'> 
+                <CardDescription title="Equipements" text="test"/>
+                <CardDescription title="Equipements" text="test"/>
+            </div>
             
         </div>
     )
