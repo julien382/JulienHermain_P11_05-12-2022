927a4ab7083fd84e207d7ecd63d70badb626aa6d
julien382 <julhermain@gmail.com>
2022-12-21 21:15:38 +0100

    rating ok, slider fonctionnel

--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -5,16 +5,13 @@ import starColor from "../../assets/starFull.svg"
 
 const Rating = (nbrStars) => {
 
-    console.log(nbrStars);
-    
     const starEmpty = <img src={star} alt="Etoile vide" />;
     const starFull = <img src={starColor} alt="Etoile complete" />;
       
     const starsArray = [];
-    console.log(starsArray);
 
     for (let i = 0; i < 5; i++) {
-        i < nbrStars ? starsArray.push(starFull) : starsArray.push(starEmpty);
+        i < nbrStars.nbrStars ? starsArray.push(starFull) : starsArray.push(starEmpty);
     }
 
     return (
