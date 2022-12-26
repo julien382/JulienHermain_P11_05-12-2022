d093e51d18bef69bc2a33bebcb9acb00abe13ab2
julien382 <julhermain@gmail.com>
2022-12-26 20:11:22 +0100

    id PP

--- src/Components/PP/PP.jsx
+++ src/Components/PP/PP.jsx
@@ -1,27 +1,13 @@
 import './PP.css'
-import data from '../../data/data.json'
-import { useParams } from 'react-router-dom'
 
-const PP = () => {
-    const {id} = useParams()
-
+const PP = (logementGoodId) => {
 
     return (
         <div className='PP'>
             <div  className="ppImage">
-                {data.map((logement, i) => (
 
-                    <>
-
-                    {(logement.id === id) && 
-
-                        <img key={i} src={logement.host.picture} alt="ppPicture" />
-
-                    }
-                    
-                    </>
-
-                ))}
+                <img src={logementGoodId.idLogement.host.picture} alt="ppPicture" />
+                
             </div>
         </div>
     )
