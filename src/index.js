98d1b9d9ad5096b5bb4e7c5a6239b38e7be4372d
julien382 <julhermain@gmail.com>
2023-01-09 20:44:53 +0100

    basename et github pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/JulienHermain_P11_05-12-2022">
       <App />
     </BrowserRouter>
   </React.StrictMode>
